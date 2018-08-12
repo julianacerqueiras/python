c5231f0944a064121388d6e4714b649165aed37a
Juliana Cerqueira <julianacerqueiras@gmail.com>
2018-08-12 19:08:11 -0300

    add edit

--- desafio26.py
+++ desafio26.py
@@ -1,2 +1,9 @@
 #Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra "A", em que posição ela aparece a primeira vez e
 # em que posição ela aparece a última vez
+
+frase = str(input('Digite uma frase:')).upper().strip()
+
+print('A frase contem {} letra(s) A!'.format(frase.count('A')))
+
+print('Primeira posicição {}'.format(frase.find('A')+1))
+print('Ultima posicição {}'.format(frase.rfind('A')+1))
--- desafio27.py
+++ desafio27.py
@@ -1,1 +1,7 @@
 #Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
+nome = str(input('Digite o seu nome completo:')).strip()
+n = nome.split()
+
+print('Primeiro nome {}:'.format(n[0]))
+print('Ultimo nome {}:'.format(n[len(n)-1]))
